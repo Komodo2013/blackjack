9b8a2f052a5dbf3da822584b9b251b01d144c4e4
Jacob Larsen <jacobm@larsen.net>
2022-11-14 10:53:27 -0700

    Changes by 10igu

--- src/BlackJack/PlayBlackJack.java
+++ src/BlackJack/PlayBlackJack.java
@@ -12,10 +12,16 @@
 
 package src.BlackJack;
 
+import src.BlackJackBase.PDeck;
 import src.BlackJackTest.CardTest;
+import src.BlackJackTest.DeckTest;
 
 public class PlayBlackJack {
     public static void main(String[] args) {
-        CardTest.run(new BlackJackCard(Face.ACE, Suit.CLUB));
+        // CardTest.run(new BlackJackCard(Face.ACE, Suit.CLUB));
+
+        //DeckTest.run();
+
+        DeckTest.run(new BlackJackDeck());
     }
 }
