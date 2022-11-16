b28011610dbcb227461783ce661172aab6818f6e
Miguel <miguel,espinoza.silloca@gmail.com>
2022-11-16 09:01:20 -0700

    BJHand incompleted

---
+++ src/BlackJack/BlackJackHand.java
@@ -0,0 +1,44 @@
+package src.BlackJack;
+
+import src.BlackJackBase.PCard;
+import src.BlackJackBase.PHand;
+
+import java.util.ArrayList;
+
+public class BlackJackHand implements PHand {
+    ArrayList<BlackJackCard> hand;
+
+    public BlackJackHand(){
+        hand = new ArrayList<BlackJackCard>();
+    }
+    @Override
+    public int getSize() {
+        return hand.size();
+    }
+
+    @Override
+    public void addCard(PCard card) {
+        hand.add((BlackJackCard) card);
+
+    }
+
+    @Override
+    public PCard getCard(int index) {
+        return hand.get(index);
+    }
+
+    @Override
+    public PCard removeCard(int index) {
+        return hand.remove(index);
+    }
+
+    @Override
+    public int getValue() {
+//        for (BlackJackCard cards : hand){
+//            if (){
+//
+//            }
+//        }
+        return 0;
+    }
+}
