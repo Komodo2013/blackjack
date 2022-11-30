28defe357850296f0f7e99d76e6d4069909d7b79
Miguel <miguel,espinoza.silloca@gmail.com>
2022-11-30 10:46:57 -0700

    pushed

--- src/draw/DrawPanel.java
+++ src/draw/DrawPanel.java
@@ -13,85 +13,93 @@ import java.awt.geom.*;
 
 /**
  * Class used to create a DrawPanel where shapes will be drawn by the programmer (via code).
- *
+ * 
  * @author Jared N. Plumb
  * @version 1.0
- * @Minor modifications by Jeff Light
  * @since 2019-11-26
+ * @Minor modifications by Jeff Light
  * @since 2019-12-01
- *  * @Minor modifications by Miguel and Jacob
- *  * @since 2019-11-26
  */
 public class DrawPanel extends JPanel {
-    //Attributes
-    private static final long serialVersionUID = 6311020027600344213L;
-
-    public DrawPanel() {
-        this.setPreferredSize(new Dimension(1600, 900));    //Sets the dimensions of the DrawPanel.  Change this if your screen doesn't support this size.
-        this.setOpaque(true);
-        this.setBackground(Color.WHITE);            //Sets the background of the DrawPanel LIGHT_GRAY.  You may change this if desired.
-    }
-
-
-    /**
-     * Overrides the JComponent.paintComponent method.
-     * EVERYTHING that gets drawn to the Component (which sits on the content frame of the JFrame) is drawn in this method.
-     * Draw12a
-     * Draw all your objects in the paintComponent method.
-     * Automatically called by the event handler whenever the screen needs to be redrawn.
-     * DO NOT CALL THIS METHOD MANUALLY
-     *
-     * @param    g    //All drawing in Java must go through a Graphics object
-     */
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;            //Allows us to draw using both the Graphics class methods and the Graphics2D class methods
-
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);    //Makes drawn shapes and fonts clearer
-
-        g2.setColor(Color.GRAY);
-        g2.setStroke(new BasicStroke(2));
-        g2.drawRect(50, 50, 200, 100);
-        g2.fillRect(300, 50, 100, 200);
-
-        g2.setColor(Color.BLUE);
-        g2.setStroke(new BasicStroke(3));
-        int[] xPoints1 = {500, 600, 600}; //Define the x cords for each point
-        int[] yPoints1 = {250, 50, 250}; //Define the y cords for each point
-        g2.drawPolygon(xPoints1, yPoints1, 3);
-        g2.fillPolygon(new int[]{700, 750, 800}, new int[]{50, 250, 50}, 3);
-
-        g2.setColor(Color.GREEN);
-        g2.setStroke(new BasicStroke(1));
-        g2.drawPolygon(new int[]{900, 950, 1050, 1000}, new int[]{50, 250, 250, 50}, 4);
-        g2.fillPolygon(new int[]{1100, 1150, 1250, 1200}, new int[]{50, 250, 250, 50}, 4);
+	//Attributes
+	private static final long serialVersionUID = 6311020027600344213L;
+	
+	//Landscape Colors
+	private final String LIGHT_SKY_BLUE = "87CEFA";
+	private final String MIDNIGHT_BLUE = "191970";
+	private final String SNOW3 = "CDC9C8";
+	private final String WHITE = "FFFFFF";
+	private final String MY_WHITE_SMOKE = "F5F5F5";
+	private final String LAWN_GREEN = "7CFC00";
+	private final String MEDIUM_SPRING_GREEN = "00FA9A";
+	private final String SPRING_GREEN = "00FF7F";
+	
+	//Tree Colors
+	private final String BROWN = "A52A2A";
+	private final String DARK_GREEN = "006400";
+	private final String FOREST_GREEN = "228B22";
+	private final String GRAY = "808080";
+	private final String GREEN = "008000";
+	
+	//House Colors
+	private final String DARK_SLATE_GRAY = "2F4F4F";
+	private final String FIREBRICK = "B22222";
+	private final String INDIAN_RED = "CD5C5C";
+	private final String MAROON = "800000";
+	private final String MOCCASIN = "FFE4B5";
+	private final String RED = "FF0000";
+	private final String SADDLE_BROWN = "8B4513";
+	private final String SIENNA = "A0522D";
+	private final String SLATE_GRAY = "708090";
+	private final String PERU = "CD853F";
+	private final String WHITE_SMOKE = "F5F5F5";
+	private final String BURLYWOOD = "DEB887";
 
 
-        g2.setColor(Color.RED);
-        g2.setStroke(new BasicStroke(4));
-        g2.drawPolygon(new int[]{50, 100, 200, 250, 200, 100}, new int[]{400, 450, 450, 400, 350, 350}, 6);
-        g2.fillPolygon(new int[]{60, 100, 200, 240, 200, 100}, new int[]{400, 440, 440, 400, 360, 360}, 6);
+	
 
-        //pentagon
+	public DrawPanel() {
+		this.setPreferredSize(new Dimension(1600, 900));	//Sets the dimensions of the DrawPanel.  Change this if your screen doesn't support this size.
+		this.setOpaque(true);
+		this.setBackground(Color.WHITE);			//Sets the background of the DrawPanel LIGHT_GRAY.  You may change this if desired.
+	}
+	/**
+	 * Overrides the JComponent.paintComponent method.
+	 * EVERYTHING that gets drawn to the Component (which sits on the content frame of the JFrame) is drawn in this method.
+	 * Draw12b
+	 * Draw all your objects here.
+	 * Automatically called by the event handler whenever the screen needs to be redrawn.
+	 * DO NOT CALL THIS METHOD MANUALLY
+	 * 
+	 * @param	g	//All drawing in Java must go through a Graphics object
+	 */
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;			//Allows us to draw using both the Graphics class methods and the Graphics2D class methods
 
-        g2.setColor(Color.YELLOW);
-        g2.setStroke(new BasicStroke(6));
-        g2.drawPolygon(new int[]{300, 350, 400, 370, 330}, new int[]{400, 350, 400, 450, 450}, 5);
-        g2.fillPolygon(new int[]{500, 550, 600, 570, 530}, new int[]{400, 350, 400, 450, 450}, 5);
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);	//Makes drawn shapes and fonts clearer
 
-        //circle
-        g2.setColor(Color.CYAN);
-        g2.setStroke(new BasicStroke(5));
-        g2.drawOval(700,350,100,100);
-        g2.fillOval(900,350,100,100);
-
-        /*
-         * Draw at least 6 shapes in BOTH draw and fill formats using methods found in the Graphics class and/or the Graphics2D class.
-         * At least one of the colors should NOT be a standard color from the COLOR palette.
-         * Be sure all shapes are located such that they can all be seen.
-         */
-
-
-    }//end of method paintComponent(Graphics)
+		//DrawTree landscape
+		g2.setColor(Color.decode("#" + LIGHT_SKY_BLUE));
+		g2.fillRect(0,0,this.getWidth(),this.getHeight());
+		
+		g2.setColor(Color.decode("#" + LAWN_GREEN));
+		g2.fillOval((-600), 350, this.getWidth() + 1200, 700);
+		
+		//DrawTree Trees
+		Tree t1 = new Tree(g2, 50, 400, 1, 3, BROWN , DARK_GREEN);
+		t1.draw();
+		
+		Tree t2 = new Tree(g2, 200, 375, 1, 5, BROWN , GRAY);
+		t2.draw();
+		
+		Tree t3 = new Tree(g2, 300, 450, 1, 4, BROWN , FOREST_GREEN);
+		t3.draw();
+		
+		
+		//Populate the scene with your own trees here!
+		
+		
+	}//end of method paintComponent(Graphics)
 
 }//end of class DrawPanel
